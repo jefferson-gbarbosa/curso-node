a0c16572c61def81d4bceb84d6316a4230bf6c03
jefferson-gbarbosa <jeffersonqx@gmail.com>
2023-12-01 15:50:43 -0300

    mysql

---
+++ 8_NODE_MYSQL/3_resgatar_dados/index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mysql = require('mysql2');
+
+const app = express();
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+)
+
+app.use(express.json())
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>{
+    res.render('home');
+});
+
+app.post('/books/insertbook', (req, res) =>{
+    const title = req.body.title;
+    const pageqty = req.body.pageqty;
+
+    const query = `INSERT INTO books (title, pageqty) VALUES ('${title}', ${pageqty})`
+
+    conn.query(query, function(err){
+        if(err){
+            console.log(err)
+        }
+
+        res.redirect('/');
+    })
+})
+
+app.get('/books', (req, res) =>{
+    const query = "SELECT * FROM books";
+
+    conn.query(query, function(err, data){
+        if(err){
+            console.log(err);
+            return
+        }
+        const books = data
+        res.render('books', { books});
+    })
+})
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'jeff1107',
+    database: 'nodemysql2',
+})
+
+conn.connect(function(err){
+    if(err){
+        console.log(err);
+    }
+    console.log('Conectou ao MySQL!');
+
+    app.listen(3000);
+})
---
+++ 8_NODE_MYSQL/6_atualizacao_de_dados/index.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mysql = require('mysql2');
+
+const app = express();
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+)
+
+app.use(express.json())
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>{
+    res.render('home');
+});
+
+app.post('/books/insertbook', (req, res) =>{
+    const title = req.body.title;
+    const pageqty = req.body.pageqty;
+
+    const query = `INSERT INTO books (title, pageqty) VALUES ('${title}', ${pageqty})`
+
+    conn.query(query, function(err){
+        if(err){
+            console.log(err)
+        }
+
+        res.redirect('/');
+    })
+})
+
+app.get('/books', (req, res) =>{
+    const query = "SELECT * FROM books";
+
+    conn.query(query, function(err, data){
+        if(err){
+            console.log(err);
+            return
+        }
+        const books = data;
+        res.render('books', { books});
+    })
+})
+
+app.get('/books/:id', (req, res)=>{
+    const id = req.params.id;
+
+    const query = `SELECT * FROM books WHERE id = ${id}`;
+    conn.query(query, function(err, data){
+        if(err){
+            console.log(err);
+            return
+        }
+
+        const book = data[0];
+
+        res.render('book', {book});
+    })
+})
+
+app.get('/books/edit/:id', (req, res)=>{
+    const id = req.params.id;
+
+    const query = `SELECT * FROM books WHERE id = ${id}`;
+    conn.query(query, function(err, data){
+        if(err){
+            console.log(err);
+            return
+        }
+
+        const book = data[0];
+
+        res.render('editbook', {book});
+    })
+})
+app.post('/books/updatebook', (req, res) =>{
+    const id = req.body.id;
+    const title = req.body.title;
+    const pageqty = req.body.pageqty;
+
+    const query = `UPDATE books SET title = '${title}', pageqty = ${pageqty} WHERE id = ${id}`
+
+    conn.query(query, function(err){
+        if(err){
+            console.log(err);
+            return
+        }
+        res.redirect('/books');
+    }) 
+})
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'jeff1107',
+    database: 'nodemysql2',
+})
+conn.connect(function(err){
+    if(err){
+        console.log(err);
+    }
+    console.log('Conectou ao MySQL!');
+
+    app.listen(3000);
+})
